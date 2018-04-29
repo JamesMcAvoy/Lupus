91969248048b87ed28b601731ec4a287d7dcd897
Blackfish <mathieu.brisset@laposte.net>
2018-04-29 02:12:06 +0200

    base bibliothèque js front-end ; connect, send WS

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Lycanthrope</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+        <style>
+        </style>
+    </head>
+    <body>
+        <p>index</p>
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+        <script src="js/game.js"></script>
+        <script>
+            Lycanthrope.connect({
+                token: 'token',
+                pseudo: 'pseudo',
+                socket: '127.0.0.1:8888'
+            })
+        </script>
+    </body>
+</html>
--- public/js/game.js
+++ public/js/game.js
@@ -2,22 +2,124 @@
  * Lycanthrope JS
  */
 
-$(() => {
+;((global, $) => {
 
-    socket.onopen = () => {
+    // Version actuelle du script
+    let version = 'alpha'
+
+    // Retourne un string formate
+    const printf = (string, args = []) => {
+        for(i in args) {
+            string = string.replace('{' + i + '}', args[i])
+        }
+        return string
+    }
+
+    // Retourne un booleen en fonction de l'existance de l'attribut
+    const isset = (attr) => {
+        return (typeof attr !== 'undefined')
+    }
+
+    // Instancie une connexion avec le serveur WS
+    // Le token dans l'objet en paramètre est un token CSRF necessaire lors de l'initiation (connexion)
+    // pour securiser l'application et le client
+    // Il est possible de passer des paramètres additionnels via l'attribut params et la route
+    // Il est aussi possible de modifier le protocole en WSS (secure)
+    // Exemple :
+    //
+    // lycanthrope.connect({
+    //      pseudo: 'user',
+    //      token: 'token',
+    //      socket: '127.0.0.1:8080',
+    //      route: '/?pseudo={0}&token={1}&room={2}',
+    //      params: ['id_room'],
+    //      protocol: 'wss'
+    // })
+    var connect = function(args) {
+        if(!isset(args.pseudo) || !isset(args.token)) {
+            console.log('Erreur lors de la connexion : pseudo/token manquant')
+        }
+
+        socket   = isset(args.socket)   ? args.socket   : '127.0.0.1:8080'
+        route    = isset(args.route)    ? args.route    : '/?pseudo={0}&token={1}'
+        protocol = isset(args.protocol) ? args.protocol : 'ws'
+
+        // Si un paramètre params et la route n'est pas celle par defaut
+        // Dans ce cas la route avec les paramètres additionnels va être creee
+        let routeArgs
+
+        if(isset(args.route) && isset(args.params)) {
+            routeArgs = [args.pseudo, args.token].concat(args.params)
+        } else {
+            routeArgs = [args.pseudo, args.token]
+        }
+
+        route = protocol+'://'+socket+printf(route, routeArgs)
+
+        try {
+            this.socket = new WebSocket(route)
+
+            this.socket.onopen    = onOpen
+            this.socket.onclose   = onClose
+            this.socket.onmessage = onMessage
+            this.socket.onerror   = onError
+        } catch(e) {
+            console.error(e, e.stack)
+        } finally {
+            this.pseudo = args.pseudo
+            this.token = args.token
+        }
+    }
+
+    // Méthodes socket asynchrones
+
+    // On open connection
+    //
+    let onOpen = function() {
 
     }
 
-    socket.onclose = () => {
+    //
+    let onClose = function() {
 
     }
 
-    socket.onmessage = (msg) => {
+    //
+    let onMessage = function(msg) {
+        console.log(msg)
+    }
+
+    //
+    let onError = function(e) {
 
     }
 
-    socket.onerror = (e) => {
-
+    // Envoi message
+    // Prend en paramètre le message et le type de message
+    // La méthode envoie un JSON parsé en string avec le pseudo et le token
+    var send = function(msg, type = 'MSG') {
+        this.socket.send(JSON.stringify({
+            msg: msg,
+            type: type,
+            user: {
+                pseudo: this.pseudo,
+                token: this.token
+            }
+        }))
     }
 
-})
+    // Main Lycanthrope proto
+    var main = {
+        // this.socket
+        // this.pseudo
+        // this.token
+        connect: connect,
+        send: send,
+
+        v: version
+    }
+
+    // Charge l'objet dans la page actuelle
+    global.prototype.Lycanthrope = main
+
+})(Window, jQuery)
