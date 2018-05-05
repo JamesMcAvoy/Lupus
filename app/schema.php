ed52128bbf00ddb0c6e3145c994b05d40632b389
Blackfish <mathieu.brisset@laposte.net>
2018-05-05 19:40:00 +0200

    app/ & configuration & dependencies

--- app/schema.php
+++ app/schema.php
@@ -5,4 +5,6 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 // Exemple
 Capsule::schema()->create('users', function($table) {
     $table->increments('id');
+    $table->string('pseudo');
+    $table->string('token');
 });
